68ea714cb355bdc91d80254821bb1d9ef3b4867c
BParethebest <45793524+fazemodz@users.noreply.github.com>
2025-03-24 10:51:48 +0000

    Update footer.jsx
    fix attempt 2

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Footer() {
     return (
-        <footer className="bg-gray-50 dark:bg-gray-900">
+        <footer className=" dark:bg-gray-900">
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <div className="flex justify-center text-teal-600 sm:justify-start dark:text-teal-300">

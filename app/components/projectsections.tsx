fcfaf5abefb8e78303d1d06b9938ee4942cc850c
BParethebest <45793524+fazemodz@users.noreply.github.com>
2025-03-28 15:35:14 +0000

    Fixed a bug

--- app/components/projectsections.tsx
+++ app/components/projectsections.tsx
@@ -14,7 +14,7 @@ const Projectsections: React.FC<Projectsectionsdata> = ({
     return (
         <>
             <section>
-                <div className="mx-auto bg-gray-900 max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+                <div className="mx-auto bg-gray-900 w-screen px-4 py-8 sm:px-6 lg:px-8">
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
                         <div>
                             <div className="max-w-lg md:max-w-none">
